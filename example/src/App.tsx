b8f5ce12d847d9ba05885c44ddca6dc43a91e550
wanghongxin <wanghongxin@movieous.video>
2020-04-30 02:38:00 +0800

    feat: example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from 'react';
 
-import { Gap } from './reactComponentLib';
+import { Gap, Input } from './reactComponentLib';
 
 const App: FC = () => {
   return (
     <>
-      <Gap py={3} px={3}>
-        Gap Component
-      </Gap>
+      <div>
+        Gap Start:
+        <Gap py={3} px={3} />
+        Gap end
+      </div>
+      <Input bgcolor="white" />
     </>
   );
 };
