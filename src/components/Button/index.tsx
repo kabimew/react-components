aebc2d124b201c2d7f01c37a905a58774cf8d821
wanghongxin <wanghongxin@movieous.video>
2020-05-18 18:33:56 +0800

    feat: theme

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,42 @@
 import styled from 'styled-components';
-import { color, ColorProps, compose } from 'styled-system';
+import { variant } from 'styled-system';
 
-type ButtonProps = ColorProps;
+type ButtonProps = {
+  color?: 'primary' | 'secondary';
+  size?: 'small' | 'medium';
+};
 
 export const Button = styled.button<ButtonProps>`
-  ${compose(color)};
+  appearance: none;
+  font-family: inherit;
+  &:focus {
+    outline: none;
+  }
+  ${variant({
+    prop: 'color',
+    variants: {
+      primary: {
+        color: 'white',
+        backgroundColor: 'primary',
+        '&:hover': {
+          backgroundColor: 'red',
+        },
+      },
+      secondary: {
+        color: 'white',
+        backgroundColor: 'secondary',
+      },
+    },
+  })};
+  ${variant({
+    prop: 'size',
+    variants: {
+      small: {
+        px: 2,
+      },
+      medium: {
+        px: 4,
+      },
+    },
+  })};
 `;
