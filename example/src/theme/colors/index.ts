205cf642803b079d7f186552475d7284e4482ea4
wanghongxin <wanghongxin@movieous.video>
2020-05-18 20:24:21 +0800

    feat: update theme

--- example/src/theme/colors/index.ts
+++ example/src/theme/colors/index.ts
@@ -1,16 +1,43 @@
-export { primary } from './primary';
-export { secondary } from './secondary';
-export { info } from './info';
-export { warning } from './warning';
-export { success } from './success';
-export { error } from './error';
-export { blue } from './blue';
-export { gray } from './gray';
-export { green } from './green';
-export { indigo } from './indigo';
-export { orange } from './orange';
-export { pink } from './pink';
-export { purple } from './purple';
-export { red } from './red';
-export { teal } from './teal';
-export { yellow } from './yellow';
+import { primary } from './primary';
+import { secondary } from './secondary';
+import { success } from './success';
+import { info } from './info';
+import { error } from './error';
+import { warning } from './warning';
+import { blue } from './blue';
+import { gray } from './gray';
+import { indigo } from './indigo';
+import { orange } from './orange';
+import { pink } from './pink';
+import { purple } from './purple';
+import { red } from './red';
+import { green } from './green';
+import { teal } from './teal';
+import { yellow } from './yellow';
+import { black } from './black';
+import { white } from './white';
+import { transparent } from './transparent';
+import { current } from './current';
+
+export const colors = {
+  primary,
+  secondary,
+  success,
+  info,
+  error,
+  warning,
+  transparent,
+  current,
+  black,
+  white,
+  blue,
+  gray,
+  green,
+  indigo,
+  orange,
+  pink,
+  purple,
+  red,
+  teal,
+  yellow,
+};
