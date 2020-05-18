ae80473e7d4ef5dd93dc81efdb7683c5a05674ed
wanghongxin <wanghongxin@movieous.video>
2020-05-18 15:14:31 +0800

    feat: fix example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,14 @@
 import React, { FC } from 'react';
-
 import { Input } from './reactComponentLib';
+import { ThemeProvider } from 'styled-components';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme();
 
 export const App: FC = () => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Input bgcolor="white" />
-    </>
+    </ThemeProvider>
   );
 };
